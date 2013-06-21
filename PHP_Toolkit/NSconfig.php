3308a24fc5e85313aae22548de548ea06adc6a5a
Ken Zhang <ken@flashbay.com>
2013-06-21 16:04:19 +0800

    Updated netsuite api to version PHPToolkit_2013_1

---
+++ PHP_Toolkit/NSconfig.php
@@ -0,0 +1,9 @@
+<?php
+$nsendpoint = "2013_1";
+$nshost = "https://webservices.netsuite.com";
+
+$nsemail = "jDoe@netsuite.com";
+$nspassword = "mySecretPwd";
+$nsrole = "3";
+$nsaccount = "MYACCT1";
+?>
